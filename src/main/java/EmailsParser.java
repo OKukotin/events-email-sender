e09fddc5621f003af1f7ec4062e42037af450711
OKukotin <oleksandr.kukotin@gmail.com>
2021-10-03 23:02:14 +0300

    Mod: EmailsParser and EmailsParser classes have been upgraded

--- src/main/java/EmailsParser.java
+++ src/main/java/EmailsParser.java
@@ -1,13 +1,25 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 public class EmailsParser {
     private String pathToData;
 
-    EmailsParser(String path) {
-        this.pathToData = path;
+    EmailsParser() {
+        this.pathToData = "../../data/listOfInvitees.csv";
     }
 
-    public String getPathToData() {
-        return pathToData;
+    public List<String[]> getEmails() throws IOException, CsvException {
+        CSVReader reader = null;
+        try {
+            reader = new CSVReader(new FileReader(this.pathToData));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return reader.readAll();
     }
-
-    public
 }
--- src/main/java/EventEmailSender.java
+++ src/main/java/EventEmailSender.java
@@ -1,6 +1,11 @@
+import javax.mail.MessagingException;
+import javax.mail.Session;
+
 public class EventEmailSender {
-    public static void main(String[] args) {
-        MailtrapConnection connect = new MailtrapConnection();
-        connect.getSession();
+    public static void main(String[] args) throws MessagingException {
+        MailtrapConnection connection = new MailtrapConnection();
+        Session sessionInstance = connection.getSession();
+        MessageGenerator msgGen = new MessageGenerator(sessionInstance);
+
     }
 }
