5b840bc5e8be22e0cd4c782237c8901f47444939
kolegran <kolegran@gmail.com>
2021-10-13 00:32:56 +0300

    Corrected typo

--- src/main/java/GmailSessionSource.java
+++ src/main/java/GmailSessionSource.java
@@ -15,7 +15,7 @@ public class GmailSessionSource {
         this.properties.put("mail.smtp.host", SmtpServerProperties.GMAIL.getHost());
         this.properties.put("mail.smtp.port", SmtpServerProperties.GMAIL.getPort());
         this.properties.put("mail.smtp.ssl.trust", SmtpServerProperties.GMAIL.getSslTrust());
-        this.properties.put("mail.smtp.starttls.enable", SmtpServerProperties.GMAIL.getStartTls());
+        this.properties.put("mail.smtp.starttls.enable", SmtpServerProperties.GMAIL.getEnableStartTls());
         this.properties.put("mail.smtp.EnableSSL.enable", SmtpServerProperties.GMAIL.getEnableSsl());
         this.authenticator = new Authenticator() {
             @Override
