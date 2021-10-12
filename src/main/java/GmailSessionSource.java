c16e911bbed447a1aade1532ccc579bc58c921b0
OKukotin <oleksandr.kukotin@gmail.com>
2021-10-12 21:32:30 +0300

    Created GmailSessionSource class

---
+++ src/main/java/GmailSessionSource.java
@@ -0,0 +1,29 @@
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import java.util.Properties;
+
+public class GmailSessionSource {
+
+    private final Properties properties;
+    private final Authenticator authenticator;
+
+    GmailSessionSource(){
+        this.properties = new Properties();
+        this.properties.put("mail.smtp.auth", SmtpServerProperties.GMAIL.getAuthorization());
+        this.properties.put("mail.smtp.starttls", SmtpServerProperties.GMAIL.getSslTrust());
+        this.properties.put("mail.smtp.host", SmtpServerProperties.GMAIL.getHost());
+        this.properties.put("mail.smtp.port", SmtpServerProperties.GMAIL.getPort());
+        this.properties.put("mail.smtp.ssl.trust", SmtpServerProperties.GMAIL.getSslTrust());
+        this.authenticator = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(SmtpServerProperties.GMAIL.getLogin(), SmtpServerProperties.GMAIL.getPassword());
+            }
+        };
+    }
+
+    public Session getSession() {
+        return Session.getInstance(this.properties, this.authenticator);
+    }
+}
