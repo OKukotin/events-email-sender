620589e085993f9cad777c7bf91cfad9564ae55f
OKukotin <oleksandr.kukotin@gmail.com>
2021-10-13 10:38:23 +0300

    Tested sending emails to real gmail addresses, deleted mailtrap session source from main func

--- src/main/java/EmailSendingApplication.java
+++ src/main/java/EmailSendingApplication.java
@@ -8,10 +8,8 @@ public class EmailSendingApplication {
         final RecipientsCsvParser parser = new RecipientsCsvParser();
         final EventMessageFactory messageFactory = new EventMessageFactory();
         final List<Recipient> recipients = parser.getRecipients();
-        final Session mailtrapSession = new MailTrapSessionSource().getSession();
         final Session gmailSession = new GmailSessionSource().getSession();
         for (Recipient recipient : recipients) {
-//            messageSender.send(messageFactory.create(mailtrapSession, recipient));
             messageSender.send(messageFactory.create(gmailSession, recipient));
         }
     }
