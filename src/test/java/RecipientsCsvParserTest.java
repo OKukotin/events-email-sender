3e83d4039c7b8f3a779c83e0f6930eed07a0ed83
kolegran <kolegran@gmail.com>
2021-11-07 22:03:24 +0200

    Added not implemented two test cases

--- src/test/java/RecipientsCsvParserTest.java
+++ src/test/java/RecipientsCsvParserTest.java
@@ -6,12 +6,22 @@ import java.util.List;
 class RecipientsCsvParserTest {
 
     @Test
-    void whenRecipientsIsPresentInTheFile_NotEmptyList() {
+    void whenRecipientsIsPresentInTheFile_PresentInTheList() {
         final RecipientsCsvParser csvParser = new RecipientsCsvParser();
 
         final List<Recipient> recipients = csvParser.getRecipients();
 
-        // TODO: add test file Recipients.csv into test/resources. Check specific recipients instead checking on empty. And rename method accordingly to changes
+        // TODO: add test file Recipients.csv into test/resources. Check specific recipients instead checking on empty
         Assertions.assertFalse(recipients.isEmpty());
+    }
+
+    @Test
+    void whenRecipientsIsNoPresentInTheFile_AbsentInTheList() {
+        // TODO: implement
+    }
+
+    @Test
+    void whenTheTitleIsPresentInTheFile_SkipTitle() {
+        // TODO: implement
     }
 }
