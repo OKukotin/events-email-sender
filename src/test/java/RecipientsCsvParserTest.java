383878f34f4f23744915e9e20367de4f216b3750
kolegran <kolegran@gmail.com>
2021-11-07 21:32:18 +0200

    Renamed test method, added final modifiers

--- src/test/java/RecipientsCsvParserTest.java
+++ src/test/java/RecipientsCsvParserTest.java
@@ -3,14 +3,14 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class RecipientsCsvParserTest {
 
     @Test
-    void getRecipientsReturnNotEmptyList() {
-        RecipientsCsvParser csvParser = new RecipientsCsvParser();
-        List<Recipient> recipients = csvParser.getRecipients();
+    void whenRecipientsIsPresentInTheFile_NotEmptyList() {
+        final RecipientsCsvParser csvParser = new RecipientsCsvParser();
+
+        final List<Recipient> recipients = csvParser.getRecipients();
+
         Assertions.assertFalse(recipients.isEmpty());
     }
 }
